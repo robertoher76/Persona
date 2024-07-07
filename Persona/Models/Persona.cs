38f46c3311567cbe8ceb52880aa31370f63f5915
Roberto <robertoher76@gmail.com>
2024-07-06 18:31:43 -0600

    Agregar archivos de proyecto.

---
+++ Persona/Models/Persona.cs
@@ -0,0 +1,12 @@
+﻿namespace Persona.Models
+{
+    public class Persona
+    {
+        public string DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string CorreoElectronico { get; set; }
+    }
+}
